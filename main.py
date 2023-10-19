ca81143291948b9cacec3ebbda0cb4d19489ffba
parthp0u0 <116512352+parthp0u0@users.noreply.github.com>
2023-10-18 20:13:43 -0500

    add line to show speed

--- main.py
+++ main.py
@@ -10,26 +10,44 @@ class Box:
         self.num = num
         self.boxSize = boxSize
         self.pos = PVector(randint(100,screenWidth-100),randint(100,screenHeight-100))
-        self.speed = PVector(randint(1,10), randint(1,10))
+        self.speed = PVector(randint(1,5), randint(1,5))
         
         #the length of the vector
         self.lineLength = self.pos.add(self.speed)
+        
+        rectMode(RADIUS)
         
     def show(self):
         #makes a rectangle
         fill('#FFFFFF')
         rect(self.pos.x, self.pos.y, self.boxSize, self.boxSize)
         fill('#F66C6C')
-        line(self.pos.x +  self.boxSize/2, self.pos.y +  self.boxSize/2, self.lineLength.x*1.1, self.lineLength.y*1.1)
+        
+        #the end point of the line
+        self.lineEndpoint = PVector(self.pos.x + self.speed.x * 10, self.pos.y + self.speed.y * 10)
+        
+        line(self.pos.x, self.pos.y, self.lineEndpoint.x, self.lineEndpoint.y)
     def update(self):
         #adds the speed to x and y position of recangle
         self.pos.add(self.speed)
         
         #bounces of the edge of the screen
-        if (self.pos.x + self.boxSize) > screenWidth or (self.pos.x) < 0:
+        if (self.pos.x + self.boxSize) > screenWidth or (self.pos.x - self.boxSize) < 0:
             self.speed.x = self.speed.x * -1
-        if (self.pos.y + self.boxSize) > screenHeight or (self.pos.y) < 0:
+        if (self.pos.y + self.boxSize) > screenHeight or (self.pos.y - self.boxSize) < 0:
             self.speed.y = self.speed.y * -1
+        
+    def collision(self):
+        for obj in boxes:
+            if obj != self:
+                
+                #calculates the distance between the two objs
+                d = dist(self.pos.x, self.pos.y, obj.pos.x, obj.pos.y)
+                
+                if d < self.boxSize + obj.boxSize:
+                    self.speed.mult(-1)
+                    obj.speed.mult(-1)
+            
     
 def setup():
     size(screenWidth, screenHeight)
@@ -41,3 +59,4 @@ def draw():
     for obj in boxes:
         obj.show()
         obj.update()
+        obj.collision()
